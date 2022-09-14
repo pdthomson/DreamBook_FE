ee73fc9d6e36105c7bd7449542c0581f1c3c2ea6
Tyler Caudill <teecaud@hotmail.com>
2022-09-14 17:20:07 -0500

    feat: users, sessions controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  resources :users
+  resources :sessions
 end
--- spec/features/welcome/welcome_spec.rb
+++ spec/features/welcome/welcome_spec.rb
@@ -8,4 +8,18 @@ RSpec.describe 'Welcome page' do
     click_on 'Home'
     expect(current_path).to eq(root_path)
   end
+
+  it 'create a dreamer link' do
+    visit root_path
+    expect(page).to have_link('Create a Dreamer')
+    click_on 'Create a Dreamer'
+    expect(current_path).to eq(new_user_path)
+  end
+
+  it 'has a link to log in' do
+    visit root_path
+    expect(page).to have_link('Log in')
+    click_on 'Log in'
+    expect(current_path).to eq(new_session_path)
+  end
 end
