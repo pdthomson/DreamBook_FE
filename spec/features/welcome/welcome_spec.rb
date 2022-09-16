9f259742499261c3a62bbf35b60a6e8f71f2b663
Bryce Simonds <bsimonds14@gmail.com>
2022-09-16 15:18:20 -0600

    Refactor: tests for logging in now utilizes OAuth

--- spec/features/welcome/welcome_spec.rb
+++ spec/features/welcome/welcome_spec.rb
@@ -1,9 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Welcome page' do
+  before(:each) do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:google_oauth2] = 
+    OmniAuth::AuthHash.new({:provider => 'google',
+                            :uid => '123545',
+                            :info => {
+                              :email => 'email@gmail.com',
+                              :first_name => 'Smudger'
+                            },
+                            :credentials => {
+                              :token => '222'
+                            }
+                          })
+  end
+
   it 'has a home link that redirects to welcome page' do
     visit root_path
-
     expect(page).to have_link('Home')
     click_on 'Home'
     expect(current_path).to eq(root_path)
@@ -20,6 +34,23 @@ RSpec.describe 'Welcome page' do
     visit root_path
     expect(page).to have_link('Log in')
     click_on 'Log in'
-    expect(current_path).to eq(new_session_path)
+    expect(current_path).to eq(dashboard_path)
+  end
+
+  it 'if logged in, no link to log in or create an account do' do
+    visit root_path
+    expect(page).to have_link('Log in')
+    click_link 'Log in'
+    expect(current_path).to be dashboard_path
+
+
+    visit root_path
+    expect(page).to_not have_link('Log in')
+    expect(page).to_not have_button('Create a Dreamer')
+    expect(page).to have_link('Log out')
+    click_link 'Log out'
+    expect(current_path).to eq(root_path)
+    expect(page).to have_link('Log in')
+    expect(page).to have_button('Create a Dreamer')
   end
 end
