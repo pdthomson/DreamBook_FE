0ae2d7dba9d2fff3acb8b18a8a4da0dc316a8e16
Bryce Simonds <bsimonds14@gmail.com>
2022-09-16 16:11:30 -0600

    Test: user show page has ome and log out link for logged in user

--- spec/features/welcome/welcome_spec.rb
+++ spec/features/welcome/welcome_spec.rb
@@ -23,32 +23,24 @@ RSpec.describe 'Welcome page' do
     expect(current_path).to eq(root_path)
   end
 
-  it 'create a dreamer link' do
+  it 'has a link to log in or register' do
     visit root_path
-    expect(page).to have_link('Create a Dreamer')
-    click_on 'Create a Dreamer'
-    expect(current_path).to eq(new_user_path)
-  end
-
-  it 'has a link to log in' do
-    visit root_path
-    expect(page).to have_link('Log in')
-    click_on 'Log in'
+    expect(page).to have_link('Log in/Register')
+    click_on 'Log in/Register'
     expect(current_path).to eq(dashboard_path)
   end
 
   it 'if logged in, no link to log in or create an account do' do
     visit root_path
-    expect(page).to have_link('Log in')
-    click_link 'Log in'
+    expect(page).to have_link('Log in/Register')
+    click_link 'Log in/Register'
     expect(current_path).to eq(dashboard_path)
     visit root_path
-    expect(page).to_not have_link('Log in')
-    expect(page).to_not have_link('Create a Dreamer')
+    expect(page).to_not have_link('Log in/Register')
     expect(page).to have_link('Log out')
     click_link 'Log out'
     expect(current_path).to eq(root_path)
-    expect(page).to have_link('Log in')
-    expect(page).to have_link('Create a Dreamer')
+    expect(page).to have_link('Log in/Register')
+
   end
 end
