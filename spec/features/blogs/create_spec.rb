1ef5331d1cf0ba76a238109e662d11bb357f736d
Bryce Simonds <bsimonds14@gmail.com>
2022-09-17 15:54:52 -0600

    Test: creates a blog in the back end

---
+++ spec/features/blogs/create_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe "Blog Create Page" do 
+  before(:each) do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:google_oauth2] = 
+    OmniAuth::AuthHash.new({:provider => 'google',
+                            :uid => '123545',
+                            :info => {
+                              :email => 'email@gmail.com',
+                              :first_name => 'Smudger'
+                            },
+                            :credentials => {
+                              :token => '222'
+                            }
+                          })
+  end
+
+  it 'creates a blog in the back end', :vcr do 
+    visit root_path
+    click_on 'Log in/Register'
+    visit '/blogs/new'
+    title = Faker::Book.title 
+    fill_in :title, with: title
+    fill_in :body, with: Faker::Fantasy::Tolkien.poem
+    select "Public", :from => "status"
+    click_on 'Submit'
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_link(title)
+  end
+end
