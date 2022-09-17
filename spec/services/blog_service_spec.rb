2beb27b6d2c8e5eb53e6fffb9ad2d092fba55481
Tyler Caudill <teecaud@hotmail.com>
2022-09-17 16:09:45 -0500

    test/feat comment service

--- spec/services/blog_service_spec.rb
+++ spec/services/blog_service_spec.rb
@@ -1,9 +1,9 @@
-require 'rails_helper' 
+require 'rails_helper'
 
 RSpec.describe BlogService do
    it 'retrieves blog data', :vcr do
-    parsed_json = BlogService.get_all_blogs 
-     expect(parsed_json).to be_a Hash 
+    parsed_json = BlogService.get_all_blogs
+     expect(parsed_json).to be_a Hash
      expect(parsed_json[:data]).to be_a Array
      first_blog = parsed_json[:data][0][:attributes]
      expect(first_blog).to include(:title, :body, :status, :user_id)
