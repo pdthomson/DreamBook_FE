1d67e6137ad2165ce64118ea15446c844c25a6b7
Tyler Caudill <teecaud@hotmail.com>
2022-09-17 21:25:46 -0500

    add blogs index, add links to blogs index

---
+++ spec/features/comments/create_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "Comments Create Page" do
+  before(:each) do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:google_oauth2] =
+    OmniAuth::AuthHash.new({:provider => 'google',
+                            :uid => '123545',
+                            :info => {
+                              :email => 'email@gmail.com',
+                              :first_name => 'Smudger'
+                            },
+                            :credentials => {
+                              :token => '222'
+                            }
+                          })
+  end
+
+  it 'creates a comment in the back end', :vcr do
+    visit root_path
+    click_on 'Log in/Register'
+    visit '/blogs/new'
+    
+  end
+end
--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -46,4 +46,18 @@ RSpec.describe 'User Show Page' do
     click_on 'Update'
     expect(current_path).to eq('/dashboard')
   end
+
+  it 'can create a public blog', :vcr do
+    visit root_path
+    click_on 'Log in/Register'
+    expect(page).to have_button('Record a Dream')
+    click_on 'Record a Dream'
+    expect(current_path).to eq(new_blog_path)
+    fill_in :title, with: 'Test'
+    fill_in :body, with: 'This is a test'
+    select 'shared', :from => 'status'
+    click_on 'Submit'
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_link('Test')
+  end
 end
